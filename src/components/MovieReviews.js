cf5965972a2d785eba76eec9c241a41c40c162f7
Jana Zrayka <janjoune.97@hotmail.com>
2021-01-18 01:23:04 +0000

    Automatically backed up by Learn

--- src/components/MovieReviews.js
+++ src/components/MovieReviews.js
@@ -1,1 +1,17 @@
 // Code MovieReviews Here
+
+import React from 'react';
+
+const review = ({reviews}) => (
+  <div className="review-list">
+  {reviews.map((review) => {
+  return(
+  <div
+      key={el.headline}
+      className="review"
+    >
+    </div>)})
+    </div>
+  )
+  
+export default MovieReviews
--- src/components/SearchableMovieReviewsContainer.js
+++ src/components/SearchableMovieReviewsContainer.js
@@ -1,9 +1,33 @@
 import React, { Component } from 'react';
 import 'isomorphic-fetch';
-import MovieReviews from './MovieReviews'
+import MovieReviews from './MovieReviews';3e4\
+
 
 const NYT_API_KEY = 'dGpQ5OmGP2SgfvZimlpCUoF4iOag9qzZ';
 const URL = 'https://api.nytimes.com/svc/movies/v2/reviews/search.json?'
             + `api-key=${NYT_API_KEY}`;
 
 // Code SearchableMovieReviewsContainer Here
+ export default class SearchableMovieReviewsContainer extends Component{
+   constructor(){
+     super();
+     this.state={
+       search:[];
+       
+     };
+   }
+   
+   componentDidMount(){
+     fetch(URL)
+     .then(res => res.json)
+    .then(data => this.setState({search :data.search}))
+   }
+   
+   render(){
+     return(
+       <div className="searchable-movie-reviews">
+       <SearchableMovieReviewsContainera search={this.state.search}/>
+       </div>
+       );
+   }
+ }
